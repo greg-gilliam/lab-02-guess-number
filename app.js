2a0dfa7e3519eb66f573767f7c5fff464a719490
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-06-30 17:55:07 -0700

    placeholder

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 // import functions and grab DOM elements
+import { compareNumbers } from './utils.js';
+const feedback = document.getElementById('feedback');
 const guessButton = document.getElementById('guess-btn');
 // initialize state
 const userGuess = document.getElementById('user-guess');
-const targetNumber = Math.floor(Math.random() * 20);  
+const targetNumber = Math.floor(Math.random() * 20) + 1;  
 let guessesRemaining = 4;
 // set event listeners 
 guessButton.addEventListener('click', ()=> {
-    console.log(userGuess, targetNumber, userGuessNum, guessesRemaining);
+    if (guessesRemaining === 0) 
+        console.log(userGuess, targetNumber, userGuessNum, guessesRemaining);
     const userGuessNum = Number(userGuess);
 });
   // get user input
--- utils.js
+++ utils.js
@@ -1,3 +1,12 @@
-export function compareNumbers(targetNumber, userGuess){
+export function compareNumbers(targetNumber, str, userGuess){
     console.log(targetNumber, userGuess);
+
+    if (targetNumber === userGuessNum) {
+        result.textContent = 'You got it! Game over!';
+    } else if (targetNumber > userGuessNum) {
+        result.textContent = 'Your guess is too high';
+    } else {
+        feedback.textContent = 'Your guess is too low';
+    }
+
 }
